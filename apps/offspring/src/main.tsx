87474854d9606b7fa5b97ef5a7102966b1a0acd2
Aaron Payne <aaronpayneconsulting@gmail.com>
2025-09-13 17:57:17 -0500

    cap error

--- apps/offspring/src/main.tsx
+++ apps/offspring/src/main.tsx
@@ -1,4 +1,4 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { AppOffspring } from "./App-offspring";
+import { AppOffspring } from "./App-Offspring";
 createRoot(document.getElementById("root")!).render(<AppOffspring />);
